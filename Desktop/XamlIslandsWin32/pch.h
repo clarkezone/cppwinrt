e8f9c0e576890d23729e8004d9c0df082d2cb6ec
James Clarke <james@clarkezone.net>
2018-06-26 13:17:12 -0700

    Add XAMLIslandsWin32 project

---
+++ Desktop/XamlIslandsWin32/pch.h
@@ -0,0 +1,11 @@
+#pragma once
+
+#include <winrt/Windows.System.h>
+#include <winrt/Windows.UI.Composition.Desktop.h>
+#include <windows.ui.composition.interop.h>
+#include <ShellScalingAPI.h>
+#include <DispatcherQueue.h>
+#include <winrt/windows.ui.xaml.hosting.h>
+#include <windows.ui.xaml.hosting.desktopwindowxamlsource.h>
+#include <winrt/windows.ui.xaml.controls.h>
+#include <winrt/Windows.UI.Xaml.Media.h>
