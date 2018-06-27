4b016c348a6d819debda5be6a3fd310072017db1
James Clarke <james@clarkezone.net>
2018-06-27 13:42:00 -0700

    Add shape morphing sample as seperate sample

--- Desktop/DesktopComposition/pch.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-#include <winrt/Windows.System.h>
-#include <winrt/Windows.UI.Composition.Desktop.h>
-#include <windows.ui.composition.interop.h>
-#include <ShellScalingAPI.h>
-#include <DispatcherQueue.h>
-#include "Generated Files/winrt/Microsoft.Graphics.Canvas.h"
-#include "Generated Files/winrt/Microsoft.Graphics.Canvas.Geometry.h"
