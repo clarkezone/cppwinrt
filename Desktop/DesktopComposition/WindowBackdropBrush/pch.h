60b465a4d67852a8871fcdd9a5e7c566fa99245f
James Clarke <james@clarkezone.net>
2018-06-27 17:55:57 -0700

    initial

---
+++ Desktop/DesktopComposition/WindowBackdropBrush/pch.h
@@ -0,0 +1,10 @@
+#pragma once
+
+#include <winrt/Windows.System.h>
+#include <winrt/Windows.UI.Composition.Desktop.h>
+#include <windows.ui.composition.interop.h>
+#include <ShellScalingAPI.h>
+#include <DispatcherQueue.h>
+//#include "Generated Files/winrt/Microsoft.Graphics.Canvas.h"
+//#include "Generated Files/winrt/Microsoft.Graphics.Canvas.Geometry.h"
+#include "Generated Files/winrt/Microsoft.Graphics.Canvas.Effects.h"
