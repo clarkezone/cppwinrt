36bb4823466808605ce678eb9c387b7c83a37f9c
James Clarke (OS GROUP) <jeclarke@ntdev.microsoft.com>
2018-06-29 07:57:43 -0700

    Fix header ordering issues

--- Desktop/XamlIslandsWin32/pch.h
+++ Desktop/XamlIslandsWin32/pch.h
@@ -1,10 +1,8 @@
 #pragma once
 
-#include <winrt/Windows.System.h>
-#include <winrt/Windows.UI.Composition.Desktop.h>
-#include <windows.ui.composition.interop.h>
 #include <ShellScalingAPI.h>
-#include <DispatcherQueue.h>
+
+#include <winrt/Windows.System.h>
 #include <winrt/windows.ui.xaml.hosting.h>
 #include <windows.ui.xaml.hosting.desktopwindowxamlsource.h>
 #include <winrt/windows.ui.xaml.controls.h>
